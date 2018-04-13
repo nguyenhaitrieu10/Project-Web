7c9061022aa685cc52cfff8232310c1f8c3e61d5
Nguyen Hai Trieu <nguyenhaitrieu10@gmail.com>
2018-04-13 09:41:13 +0700

    project 4

---
+++ Project4/js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+	var totalWidth = 0;
+	var positions = new Array();
+
+	$('.slides .slide').each(function(i){
+		positions[i] = totalWidth;
+		totalWidth += $(this).width();
+
+		if (!$(this).width()){
+			alert('Please add a width to your images');
+			return false;
+		}
+	});
+
+	$('.slides').width(totalWidth);
+
+	$('#menu ul li a').click(function(e, keepScroll){
+		$('li.product').removeClass('active').addClass('inactive');
+		// Add active class
+		$(this).parent().addClass('active');
+
+		var pos = $(this).parent().prevAll('.product').length;
+		 $('.slides').stop().animate({marginLeft: -positions[pos] + 'px'},450);
+
+		 e.preventDefault();
+
+		 if (!autoScroll)
+		 	clearInterval(itvl);
+	});
+
+	//Make first image active
+	$('#menu ul li.product:first').addClass('active');
+
+	// Auto Scroll
+	var current = 1;
+	function autoScroll(){
+		if (current == -1)
+			return false;
+
+		$('#menu ul li a').eq(current%$('#menu ul li a').length).trigger('click',[true]);
+		current++;
+	}
+
+
+	//Duration for auto scroll
+	var duration = 2;
+	var itvl = setInterval(function(){autoScroll()},duration*1000);
+});
